11a140503115d8c122ea6561fddbd5bec14aa814
pinwei wu <pw.amy.wu@gmail.com>
2020-11-30 10:57:24 -0600

    add joi hearers authorization

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const mainRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middleware/auth');
 const userRouter = require('./user');
 const articleRouter = require('./article');
 
-mainRouter.use('/users', auth, userRouter);
-mainRouter.use('/articles', auth, articleRouter);
+mainRouter.use('/users', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), auth, userRouter);
+mainRouter.use('/articles', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }),
+}), auth, articleRouter);
 
 module.exports = mainRouter;
