feeef5872f379249087cec037e192357d5236fd2
pinwei wu <pw.amy.wu@gmail.com>
2020-11-30 13:34:59 -0600

    delete header joi check

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,7 @@ const auth = require('../middleware/auth');
 const userRouter = require('./user');
 const articleRouter = require('./article');
 
-mainRouter.use('/users', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
-  }),
-}), auth, userRouter);
-mainRouter.use('/articles', celebrate({
-  headers: Joi.object().keys({
-    authorization: Joi.string().required(),
-  }),
-}), auth, articleRouter);
+mainRouter.use('/users', auth, userRouter);
+mainRouter.use('/articles', auth, articleRouter);
 
 module.exports = mainRouter;
